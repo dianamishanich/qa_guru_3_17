d1ee705aea972b0d233735282845064035692016
dianamishanich <diana-mishanich@ya.ru>
2021-02-19 00:58:58 +0300

    DemoWebShopTests

--- src/test/java/DemoWebShopTests.java
+++ src/test/java/DemoWebShopTests.java
@@ -1,4 +1,5 @@
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -7,56 +8,39 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DemoWebShopTests {
 
-
-    @Test
-    void addToWishlistTest() {
         String body = "addtocart_51.EnteredQuantity=1";
         String cookie = "Nop.customer=8ceb6c9b-a5bf-4f8f-8900-b9e59bab7be6; __utmz=78382081.1611734283.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); nop.CompareProducts=; NopCommerce.RecentlyViewedProducts=RecentlyViewedProductIds=51&RecentlyViewedProductIds=29&RecentlyViewedProductIds=53&RecentlyViewedProductIds=14; ARRAffinity=7f10010dd6b12d83d6aefe199065b2e8fe0d0850a7df2983b482815225e42439; __utmc=78382081; __utma=78382081.1191995250.1611734283.1612630814.1612646290.7; __utmt=1; __atuvc=1%7C4%2C22%7C5%2C1%7C6; __atuvs=601f07b0b48db0da000; __utmb=78382081.7.10.1612646290";
+        String path = "http://demowebshop.tricentis.com/addproducttocart/details/51/2";
 
-        given()
-                .body(body)
-                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .cookie(cookie)
-                .when()
-                .post("http://demowebshop.tricentis.com/addproducttocart/details/51/2")
-                .then()
-                .statusCode(200);
+        ValidatableResponse makeRequest() {
+            return given()
+                    .body(body)
+                    .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                    .cookie(cookie)
+                    .when()
+                    .post(path)
+                    .then()
+                    .statusCode(200);
+
     }
 
     @Test
     void addToWishlistAssertTest() {
-        String body = "addtocart_51.EnteredQuantity=1";
-        String cookie = "Nop.customer=8ceb6c9b-a5bf-4f8f-8900-b9e59bab7be6; __utmz=78382081.1611734283.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); nop.CompareProducts=; NopCommerce.RecentlyViewedProducts=RecentlyViewedProductIds=51&RecentlyViewedProductIds=29&RecentlyViewedProductIds=53&RecentlyViewedProductIds=14; ARRAffinity=7f10010dd6b12d83d6aefe199065b2e8fe0d0850a7df2983b482815225e42439; __utmc=78382081; __utma=78382081.1191995250.1611734283.1612630814.1612646290.7; __utmt=1; __atuvc=1%7C4%2C22%7C5%2C1%7C6; __atuvs=601f07b0b48db0da000; __utmb=78382081.7.10.1612646290";
 
-        Response response = given()
-                .body(body)
-                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .cookie(cookie)
-                .when()
-                .post("http://demowebshop.tricentis.com/addproducttocart/details/51/2")
-                .then()
-                .statusCode(200)
-                .extract().response();
+            Response response = makeRequest()
+                    .extract()
+                    .response();
 
-        assertTrue(response.asString().contains("The product has been added to your"));
+            assertTrue(response.asString().contains("The product has been added to your"));
 
     }
 
     @Test
     void addToWishlistBodyAssertTest() {
-        String body = "addtocart_51.EnteredQuantity=1";
-        String cookie = "Nop.customer=8ceb6c9b-a5bf-4f8f-8900-b9e59bab7be6; __utmz=78382081.1611734283.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); nop.CompareProducts=; NopCommerce.RecentlyViewedProducts=RecentlyViewedProductIds=51&RecentlyViewedProductIds=29&RecentlyViewedProductIds=53&RecentlyViewedProductIds=14; ARRAffinity=7f10010dd6b12d83d6aefe199065b2e8fe0d0850a7df2983b482815225e42439; __utmc=78382081; __utma=78382081.1191995250.1611734283.1612630814.1612646290.7; __utmt=1; __atuvc=1%7C4%2C22%7C5%2C1%7C6; __atuvs=601f07b0b48db0da000; __utmb=78382081.7.10.1612646290";
 
-        given()
-                .body(body)
-                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .cookie(cookie)
-                .when()
-                .post("http://demowebshop.tricentis.com/addproducttocart/details/51/2")
-                .then()
-                .statusCode(200)
-                .body("success", is(true))
-                .body("message", is("The product has been added to your <a href=\"/wishlist\">wishlist</a>"));
+            makeRequest()
+                    .body("success", is(true))
+                    .body("message", is("The product has been added to your <a href=\"/wishlist\">wishlist</a>"));
 
     }
 
